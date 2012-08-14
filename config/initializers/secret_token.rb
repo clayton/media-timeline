c837b95c94169bb6c8201981b68f857812f35706
Clayton <clayton@claytonlz.com>
2012-08-14 16:45:19 -0700

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediaTimeline::Application.config.secret_token = '64a807aa56dae6bdc21656d0a250c80a6dbce1bdd5350082e6f947a0792e4c58a8f4ed77785be8f14ef9b8cb1e7c9f5fcc7780c138dd675d762ef810c7cbffd5'
