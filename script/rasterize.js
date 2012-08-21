f8891d6f2f3b872a9b2c83a2ce24a996b8734ee9
Clayton <clayton@claytonlz.com>
2012-08-21 14:40:38 -0700

    Click 0,0 on the page so that modal adverts go away

--- script/rasterize.js
+++ script/rasterize.js
@@ -15,6 +15,10 @@ if (phantom.args.length < 3 || phantom.args.length > 4) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
         } else {
+            window.setTimeout(function(){
+                page.sendEvent("click", 0,0);
+            }, 1000);
+
             window.setTimeout(function () {
                 page.render(png_out);
                 page_source = page.evaluate(function () {
